a3c0f3afabb61ecaa0507de62e2d2fcb87ebe775
ajsaenz1091 <ajsaenz1091@gmail.com>
2020-11-21 23:40:12 -0500

    fixed favourites component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
-import AddFavourites from './components/AddFavourites';
+import AddFavourites from './components/AddToFavourites';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
